4ac6d5e86c692c60849391a0a77c80f86dd1bdb8
DavidPalomeque_zebraAccount_ <davidpalomequetech@gmail.com>
2022-07-17 14:41:10 -0300

    add: scrap news headlines;

---
+++ news-headlines.py
@@ -0,0 +1,36 @@
+from http import server
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import pandas as pa
+
+website = "https://www.thesun.co.uk/sport/football/"
+path = r"C:\Users\Usuario\Downloads\chromedriver.exe" # here goes your chromedriver location
+
+# opens website in chromedriver
+service = Service(executable_path=path)
+driver = webdriver.Chrome(service=service)
+driver.get(website)
+
+# web scraping with xpath
+containers = driver.find_elements(by="xpath",value='//div[@class="teaser__copy-container"]')
+titles = []
+subtitles = []
+links = []
+
+for container in containers:
+    title = container.find_element(by='xpath', value='./a/h2').text # by './' we avoid writing '//div[@class="teaser__copy-container"]' again
+    subtitle = container.find_element(by='xpath', value='./a/p').text 
+    link = container.find_element(by='xpath', value='./a').get_attribute('href')
+    titles.append(title)
+    subtitles.append(subtitle)
+    links.append(link)
+
+print(titles)
+print(subtitles)
+print(links)
+
+my_dict = {'title': titles, 'subtitle':subtitles, 'link':links}
+news = pa.DataFrame(my_dict)
+news.to_csv("news.csv")
+
+driver.quit()
